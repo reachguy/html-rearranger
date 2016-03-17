3c3335dbdb685b3c6ada1ddeb06b629cd8e16053
Shogo Shimizu <shogoluc@gmail.com>
2016-03-17 15:36:17 -0400

    added content

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Create a heading: “Below ground veggies”.
+// Create a list to hold the below ground veggies.
+// Add the heading and list to the document.
+var $belowUl = $('<ul>');
+var $aboveUl = $('<ul>');
+
+$('body').append('<h1>Below ground veggies</h1>');
+$('body').append($belowUl);
+
+$('body').append('<h1>Above ground veggies</h1>');
+$('body').append($aboveUl);
+
+$('ul li').each(function () {
+  if ($(this).hasClass('below')){
+    $belowUl.append($(this));
+} else {
+    $aboveUl.append($(this));
+  }
+});
+
+
+
